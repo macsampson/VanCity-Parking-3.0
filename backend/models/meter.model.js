245ba89de72cb14466faf44fe7a2812bdefe55c0
Mac Sampson <mackenzie.sampson@outlook.com>
2019-05-09 20:08:19 -0700

    Update schema to represent GeoJSON

--- backend/models/meter.model.js
+++ backend/models/meter.model.js
@@ -2,46 +2,51 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 const meterSchema = new Schema({
-  meter_type: String,
-  meter_id: String,
-  limits_and_rates: {
-    weekdays: [
-      {
-        start: String,
-        end: String,
-        rate: String,
-        hour_limit: String
-      }
-    ],
-    saturday: [
-      {
-        start: String,
-        end: String,
-        rate: String,
-        hour_limit: String
-      }
-    ],
-    sunday: [
-      {
-        start: String,
-        end: String,
-        rate: String,
-        hour_limit: String
-      }
-    ],
-    time_misc: String,
-    rate_misc: String
+  type: { type: String },
+  geometry: {
+    type: { type: String },
+    coordinates: { type: [Number], index: "2dsphere", required: true }
   },
-  pay_by_phone_num: String,
-  in_effect: {
-    start: String,
-    end: String
-  },
-  credit_card: String,
-  prohibitions: String,
-  lat: Number,
-  lng: Number,
-  date_updated: String
+  properties: {
+    meter_type: String,
+    meter_id: String,
+    limits_and_rates: {
+      weekdays: [
+        {
+          start: String,
+          end: String,
+          rate: String,
+          hour_limit: String
+        }
+      ],
+      saturday: [
+        {
+          start: String,
+          end: String,
+          rate: String,
+          hour_limit: String
+        }
+      ],
+      sunday: [
+        {
+          start: String,
+          end: String,
+          rate: String,
+          hour_limit: String
+        }
+      ],
+      time_misc: String,
+      rate_misc: String
+    },
+    pay_by_phone_num: String,
+    in_effect: {
+      start: String,
+      end: String
+    },
+    credit_card: String,
+    prohibitions: String,
+    date_updated: String
+  }
 });
 
 var Meter = mongoose.model("meters", meterSchema);
