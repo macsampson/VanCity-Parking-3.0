185055411a924a92348b4c9b359dfbb766063d0d
Mac Sampson <mackenzie.sampson@outlook.com>
2019-05-09 20:07:44 -0700

    Update loadMeters to save in GeoJSON format

--- backend/db.js
+++ backend/db.js
@@ -6,11 +6,16 @@ var unzip = require("unzip");
 var fstream = require("fstream");
 var convert = require("xml-js");
 
-// Connect to Mongo on start
-mongoose.connect("mongodb://localhost/parker", { useNewUrlParser: true });
+var meter = require("./models/meter.model");
 
-const connection = mongoose.connection;
-connection.once("open", () => {
+// Connect to Mongo on start
+mongoose.connect("mongodb://localhost/parker", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
@@ -45,173 +50,205 @@ const kml = new DOMParser().parseFromString(
 const converted = tj.kml(kml);
 const geojson = tj.kml(kml, { styles: true });
 
+// Write json to file
+fs.writeFile(
+  "./data/parking_meters_dirty.json",
+  JSON.stringify(geojson),
+  "utf8",
+  // callback function that is called after writing file is done
+  function(err) {
+    if (err) throw err;
+    // if no error
+    console.log(
+      "Parking meter converted to GeoJSON and written to file successfully."
+    );
+  }
+);
+
 var parking_meters = {
-  pm: []
+  type: "FeatureCollection",
+  features: []
 };
 
 pms = geojson.features;
 
 for (var i in pms) {
   var pm = pms[i];
-  if (pm.geometry.type == "Point") {
-    const na = "N/A";
-    const type_regex = /(?<=Type:\s\n).+/gm;
-    head_type = pm.properties.name;
-    var type_exists = type_regex.test(head_type);
+  var is_double = pm.geometry.type == "GeometryCollection";
+  const na = "N/A";
+  const type_regex = /(?<=Type:\s\n).+/gm;
+  head_type = pm.properties.name;
+  var type_exists = type_regex.test(head_type);
 
-    if (type_exists) {
-      type = head_type.match(type_regex)[0];
-    } else {
-      type = "N/A";
-    }
+  if (type_exists) {
+    type = head_type.match(type_regex)[0];
+  } else {
+    type = "N/A";
+  }
 
-    // Set some regex for use later on
-    const time_limit_regex = /(?<=Time Limits:\s\n<br>)[^]+(?=\n<br>Parking)/gm;
-    const rates_and_days_regex = /(?<=Rates:\s\n<br>)[^]+\$[0-9]*\.[0-9]*/gm;
+  // Set some regex for use later on
+  const time_limit_regex = /(?<=Time Limits:\s\n<br>)[^]+(?=\n<br>Parking)/gm;
+  const rates_and_days_regex = /(?<=Rates:\s\n<br>)[^]+\$[0-9]*\.[0-9]*/gm;
 
-    const weekday_regex = /M-F/;
-    const sat_regex = /Sat/;
-    const sun_regex = /Sun/;
+  const weekday_regex = /M-F/;
+  const sat_regex = /Sat/;
+  const sun_regex = /Sun/;
 
-    const rate_regex = /(\$\s?.*)/gm;
+  const rate_regex = /(\$\s?.*)/gm;
 
-    var desc = pm.properties.description;
+  var desc = pm.properties.description;
 
-    var time_limits_exist = time_limit_regex.test(desc);
-    var rates_and_days_exist = rates_and_days_regex.test(desc);
+  var time_limits_exist = time_limit_regex.test(desc);
+  var rates_and_days_exist = rates_and_days_regex.test(desc);
 
-    var weekday_info = [];
-    var sat_info = [];
-    var sun_info = [];
-    var time_misc = "";
-    var rate_misc = "";
+  var weekday_info = [];
+  var sat_info = [];
+  var sun_info = [];
+  var time_misc = "";
+  var rate_misc = "";
 
-    // Create an array containing the days, times, and corresponding rates
-    if (rates_and_days_exist) {
-      var rate_day_array = desc
-        .match(rates_and_days_regex)[0]
-        .trim()
-        .split("\n<br>");
-    }
+  // Create an array containing the days, times, and corresponding rates
+  if (rates_and_days_exist) {
+    var rate_day_array = desc
+      .match(rates_and_days_regex)[0]
+      .trim()
+      .split("\n<br>");
+  }
 
-    if (time_limits_exist) {
-      // Create an array containing the days, times, and corresponding time limits
-      var time_limits_array = desc
-        .match(time_limit_regex)[0]
-        .replace(/\n/g, "")
-        .split("<br>");
-      for (var i in time_limits_array) {
-        // console.log(time_limits_array[i]);
-        var day_info = [];
-        // Determine what day the current time limits are for
-        if (weekday_regex.test(time_limits_array[i])) {
-          day_info = weekday_info;
-        } else if (sat_regex.test(time_limits_array[i])) {
-          day_info = sat_info;
-        } else if (sun_regex.test(time_limits_array[i])) {
-          day_info = sun_info;
-        } else if (/Miscellaneous:/.test(time_limits_array[i])) {
-          time_misc = time_limits_array[i].match(/(?<=Miscellaneous:\s).*/)[0];
-        }
-        // Parse out the time at which rates/limits come into effect
-        start = /(?<=\w+\s)\d.+(?=\sto)/.test(time_limits_array[i])
-          ? time_limits_array[i].match(/(?<=\w+\s)\d.+(?=\sto)/)[0]
-          : "N/A";
-        // Parse out the time at which rates/limits are no longer in effect
-        end = /(?<=to\s).*(?=:)/.test(time_limits_array[i])
-          ? time_limits_array[i].match(/(?<=to\s).*(?=:)/)[0]
-          : "N/A";
-
-        // Parse out the parking time limit in hours, or set to "No Time Limit" if there are none
-        if (/No Time Limit/.test(time_limits_array[i])) {
-          limit = "No Time Limit";
-        } else {
-          limit = /(?<=:\s*).*(?=\s*Hr)/.test(time_limits_array[i])
-            ? time_limits_array[i].match(/(?<=:\s*).*(?=\s*Hr)/)[0].trim()
-            : "N/A";
-        }
-        // Parse out array of rates and their corresponding days
-        if (rates_and_days_exist) {
-          if (/Miscellaneous:/.test(rate_day_array[i])) {
-            rate_misc = rate_day_array[i].match(/(?<=Miscellaneous:\s).*/)[0];
-          } else if (rate_regex.test(rate_day_array[i])) {
-            // console.log(rate_day_array[i]);
-            rate = rate_day_array[i].match(rate_regex)[0];
-          }
-        }
-        var info = {
-          start: start,
-          end: end,
-          rate: rate,
-          hour_limit: limit
-        };
-        day_info.push(info);
-        // console.log(info);
+  if (time_limits_exist) {
+    // Create an array containing the days, times, and corresponding time limits
+    var time_limits_array = desc
+      .match(time_limit_regex)[0]
+      .replace(/\n/g, "")
+      .split("<br>");
+    for (var i in time_limits_array) {
+      // console.log(time_limits_array[i]);
+      var day_info = [];
+      // Determine what day the current time limits are for
+      if (weekday_regex.test(time_limits_array[i])) {
+        day_info = weekday_info;
+      } else if (sat_regex.test(time_limits_array[i])) {
+        day_info = sat_info;
+      } else if (sun_regex.test(time_limits_array[i])) {
+        day_info = sun_info;
+      } else if (/Miscellaneous:/.test(time_limits_array[i])) {
+        time_misc = time_limits_array[i].match(/(?<=Miscellaneous:\s).*/)[0];
       }
-    }
-
-    // Check if the parking meter accepts pay-by-phone, and if so, parse number
-    const pay_regex = /(?<=Pay by Phone Number:\s)[0-9]+/gm;
-    var pay_exists = pay_regex.test(desc);
-
-    if (pay_exists) {
-      pay_by_phone_num = desc.match(pay_regex)[0];
-    } else {
-      pay_by_phone_num = "N/A";
-    }
-
-    // Parse the unique meter ID
-    const id_regex = /(?<=Meter Id:\s).*/gm;
-    var id_exists = id_regex.test(desc);
-
-    if (id_exists) {
-      meter_id = desc.match(id_regex)[0];
-    } else {
-      meter_id = "N/A";
-    }
-
-    // Parse out the hours in which the parking meter is in effect
-    const in_effect_regex = /(?<=Effect:\s).*/gm;
-    var in_effect_exists = in_effect_regex.test(desc);
-
-    if (in_effect_exists) {
-      in_effect = desc.match(in_effect_regex)[0];
-      start = /\d.*(?=\sTO)/.test(in_effect)
-        ? in_effect.match(/\d.*(?=\sTO)/)[0]
+      // Parse out the time at which rates/limits come into effect
+      start = /(?<=\w+\s)\d.+(?=\sto)/.test(time_limits_array[i])
+        ? time_limits_array[i].match(/(?<=\w+\s)\d.+(?=\sto)/)[0]
         : "N/A";
-      end = /\d.*(?=\sTO)/.test(in_effect)
-        ? in_effect.match(/(?<=TO\s)\d.*/)[0]
+      // Parse out the time at which rates/limits are no longer in effect
+      end = /(?<=to\s).*(?=:)/.test(time_limits_array[i])
+        ? time_limits_array[i].match(/(?<=to\s).*(?=:)/)[0]
         : "N/A";
-    } else {
-      in_effect = "N/A";
+
+      // Parse out the parking time limit in hours, or set to "No Time Limit" if there are none
+      if (/No Time Limit/.test(time_limits_array[i])) {
+        limit = "No Time Limit";
+      } else {
+        limit = /(?<=:\s*).*(?=\s*Hr)/.test(time_limits_array[i])
+          ? time_limits_array[i].match(/(?<=:\s*).*(?=\s*Hr)/)[0].trim()
+          : "N/A";
+      }
+      // Parse out array of rates and their corresponding days
+      if (rates_and_days_exist) {
+        if (/Miscellaneous:/.test(rate_day_array[i])) {
+          rate_misc = rate_day_array[i].match(/(?<=Miscellaneous:\s).*/)[0];
+        } else if (rate_regex.test(rate_day_array[i])) {
+          // console.log(rate_day_array[i]);
+          rate = rate_day_array[i].match(rate_regex)[0];
+        }
+      }
+      var info = {
+        start: start,
+        end: end,
+        rate: rate,
+        hour_limit: limit
+      };
+      day_info.push(info);
+      // console.log(info);
     }
+  }
 
-    // Check if the parking meters accept credit cards
-    const credit_regex = /(?<=Credit Card Enabled:\s).+(?=<br>)/gm;
-    var credit_exists = credit_regex.test(desc);
+  // Check if the parking meter accepts pay-by-phone, and if so, parse number
+  const pay_regex = /(?<=Pay by Phone Number:\s)[0-9]+/gm;
+  var pay_exists = pay_regex.test(desc);
 
-    if (credit_exists) {
-      credit = desc.match(credit_regex)[0];
-    } else {
-      credit = "No";
-    }
+  if (pay_exists) {
+    pay_by_phone_num = desc.match(pay_regex)[0];
+  } else {
+    pay_by_phone_num = "N/A";
+  }
 
-    // Check if there are any parking prohibitions
-    const prohibtion_regex = /(?<=Prohibitions:\s).+(?=<br>Credit)/gm;
-    var prohibtion_exists = prohibtion_regex.test(desc);
+  // Parse the unique meter ID
+  const id_regex = /(?<=Meter\s?Id:\s).*/gm;
+  var id_exists = id_regex.test(desc);
 
-    if (prohibtion_exists) {
-      prohibitions = desc.match(prohibtion_regex)[0];
-    } else {
-      prohibitions = "N/A";
-    }
+  if (id_exists) {
+    meter_id = desc.match(id_regex)[0];
+  } else {
+    meter_id = "N/A";
+  }
 
-    // Create parking meter object and push onto parking meters array
-    parking_meters.pm.push({
+  // Parse out the hours in which the parking meter is in effect
+  const in_effect_regex = /(?<=Effect:\s).*/gm;
+  var in_effect_exists = in_effect_regex.test(desc);
+
+  if (in_effect_exists) {
+    in_effect = desc.match(in_effect_regex)[0];
+    start = /\d.*(?=\sTO)/.test(in_effect)
+      ? in_effect.match(/\d.*(?=\sTO)/)[0]
+      : "N/A";
+    end = /\d.*(?=\sTO)/.test(in_effect)
+      ? in_effect.match(/(?<=TO\s)\d.*/)[0]
+      : "N/A";
+  } else {
+    in_effect = "N/A";
+  }
+
+  // Check if the parking meters accept credit cards
+  const credit_regex = /(?<=Credit Card Enabled:\s).+(?=<br>)/gm;
+  var credit_exists = credit_regex.test(desc);
+
+  if (credit_exists) {
+    credit = desc.match(credit_regex)[0];
+  } else {
+    credit = "No";
+  }
+
+  // Check if there are any parking prohibitions
+  const prohibtion_regex = /(?<=Prohibitions:\s).+(?=<br>Credit)/gm;
+  var prohibtion_exists = prohibtion_regex.test(desc);
+
+  if (prohibtion_exists) {
+    prohibitions = desc.match(prohibtion_regex)[0];
+  } else {
+    prohibitions = "No";
+  }
+
+  // Check if meter is a single or double head
+  // Double head meters contain two duplicate coordinates (one for each head), so we only take first set of coordinates (i.e geometries[0])
+  lat = is_double
+    ? pm.geometry.geometries[0].coordinates[1]
+    : pm.geometry.coordinates[1];
+
+  lng = is_double
+    ? pm.geometry.geometries[0].coordinates[0]
+    : pm.geometry.coordinates[0];
+
+  // Create parking meter object and push onto parking meters array
+  parking_meters.features.push({
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [lng, lat]
+    },
+    properties: {
       meter_type: type,
       meter_id: meter_id,
       limits_and_rates: {
-        weekdays: weekday_info,
+        weekdays: weekday_info, // These day data are already in array format
         saturday: sat_info,
         sunday: sun_info,
         time_misc: time_misc,
@@ -224,31 +261,27 @@ for (var i in pms) {
       },
       credit_card: credit,
       prohibitions: prohibitions,
-      lat: pm.geometry.coordinates[1],
-      lng: pm.geometry.coordinates[0],
       date_updated: Date()
-    });
-  }
+    }
+  });
 }
 
 // Print out the parking meter for testing purposes
-// console.log(parking_meters);
+// console.log(parking_object);
 
 // Turn the json object into a string to get ready for writing to a file
 const geojsonstring = JSON.stringify(parking_meters);
 
 // Write json to file
 fs.writeFile(
-  "./data/parking_meters.json",
+  "./data/parking_meters_clean.json",
   geojsonstring,
   "utf8",
   // callback function that is called after writing file is done
   function(err) {
     if (err) throw err;
     // if no error
-    console.log(
-      "Parking meter converted to GeoJSON and written to file successfully."
-    );
+    console.log("Parking meter data cleaned and written to file successfully.");
   }
 );
 
@@ -291,3 +324,50 @@ fs.writeFile(
 //     unzip.Extract({ path: "./data/" })
 //   );
 // });
+
+const Meter = require("./models/meter.model");
+
+// Function to check if a meter exists with a given id...if it does, update record with newly pulled info, otherwise create it
+async function loadMeters() {
+  for (var i in parking_meters.features) {
+    var new_meter = parking_meters.features[i];
+    Meter.updateOne(
+      { "properties.meter_id": new_meter.properties.meter_id },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [
+            new_meter.geometry.coordinates[0],
+            new_meter.geometry.coordinates[1]
+          ]
+        },
+        properties: {
+          meter_type: new_meter.properties.meter_type,
+          meter_id: new_meter.properties.meter_id,
+          limits_and_rates: {
+            weekdays: new_meter.properties.limits_and_rates.weekdays,
+            saturday: new_meter.properties.limits_and_rates.saturday,
+            sunday: new_meter.properties.limits_and_rates.sunday,
+            time_misc: new_meter.properties.limits_and_rates.time_misc,
+            rate_misc: new_meter.properties.limits_and_rates.rate_misc
+          },
+          pay_by_phone_num: new_meter.properties.pay_by_phone_num,
+          in_effect: {
+            start: new_meter.properties.in_effect.start,
+            end: new_meter.properties.in_effect.end
+          },
+          credit_card: new_meter.properties.credit_card,
+          prohibitions: new_meter.properties.prohibitions,
+          date_updated: Date()
+        }
+      },
+      { upsert: true, multi: true }
+    ).catch(err => {
+      console.log(err);
+    });
+  }
+  console.log("Parking meters saved to Mongo in GeoJSON format");
+}
+
+loadMeters();
