25f48a989e970b50f5822d5e1a2e0a26e0a4adcb
Mac Sampson <mackenzie.sampson@outlook.com>
2019-05-24 21:42:30 -0700

    Add ability to pass lat,lng, and ddistance params to api endpoint that queries meters db

--- backend/routes/meters.js
+++ backend/routes/meters.js
@@ -3,15 +3,34 @@
 var Express = require("express");
 const router = Express.Router();
 var Meter = require("../models/meter.model");
+var dbOperations = require("../db");
 
 /* GET meters listing. */
-router.get("/", (req, res, next) => {
-  Meter.find((err, meters) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(meters);
+router.get("/", (req, res) => {
+  var lat = req.query.lat;
+  var lng = req.query.lng;
+  var distance = req.query.distance;
+
+  // dbOperations.fetchNearestMeters([lng, lat], distance, function(
+  //   results
+  // ) {
+  //   // Return the results of the db qeury back in JSON form
+  //   res.json(results);
+  // });
+
+  Meter.find({
+    geometry: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [lng, lat]
+        },
+        $maxDistance: distance
+      }
     }
+  }).find((err, results) => {
+    if (err) console.log(err);
+    res.json(results);
   });
 });
 
