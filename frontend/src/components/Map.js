448587ac2439edb057ef2068edb1bcd363fed061
Mac Sampson <mackenzie.sampson@outlook.com>
2019-05-30 12:46:05 -0700

    Add favicons and brand image

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import SearchBar from "material-ui-search-bar";
+import ReactDOMServer from "react-dom/server";
+import InfoWindow from "./InfoWindow";
+import { Navbar, Form, FormControl } from "react-bootstrap";
 
 var map;
 var markers = [];
@@ -18,17 +20,36 @@ class Map extends Component {
   }
 
   render() {
+    var style = {
+      width: "100%"
+    };
+    var brandStyle = {
+      margin: "0px 15px 0px 0px"
+    };
     return (
       <div>
-        <SearchBar
-          id="autocomplete"
-          placeholder="Search..."
-          value=""
-          style={{
-            margin: "0 auto",
-            maxWidth: "100vw"
-          }}
-        />
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/parking-favicon-96.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              style={brandStyle}
+            />
+            Parking Spot Finder
+          </Navbar.Brand>
+          <Form inline style={style}>
+            <FormControl
+              id="autocomplete"
+              type="text"
+              placeholder="Search..."
+              className="mr-sm-2"
+              style={style}
+            />
+          </Form>
+        </Navbar>
         <div id="map" />
       </div>
     );
@@ -41,7 +62,9 @@ class Map extends Component {
   initMap() {
     map = new window.google.maps.Map(document.getElementById("map"), {
       center: { lat: 49.24966, lng: -123.11934 },
-      zoom: 13
+      zoom: 13,
+      gestureHandling: "greedy",
+      fullscreenControl: false
     });
 
     // this.setState({ map: map });
@@ -63,8 +86,8 @@ class Map extends Component {
 
     // Re-center and zoom the map to the location entered in the search box
     autocomplete.addListener("place_changed", function() {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
+      for (let marker of markers) {
+        marker.setMap(null);
       }
       markers = [];
       var place = autocomplete.getPlace();
@@ -86,6 +109,7 @@ class Map extends Component {
       // Construct url with query params to pass to the fetch function
       var search_lat = place.geometry.location.lat();
       var search_lng = place.geometry.location.lng();
+      // TODO: Change the domain and port before deploying to prod
       var url = new URL("http://localhost:3001/meters"),
         params = { lat: search_lat, lng: search_lng, distance: 100 };
       Object.keys(params).forEach(key =>
@@ -93,6 +117,14 @@ class Map extends Component {
       );
 
       getMeters(url).catch(err => console.log("Fetch Error : -S", err));
+
+      // TODO: Adjust map bounds to better fit all markers
+      // var bounds = new window.google.maps.LatLngBounds();
+      // for (let marker of markers) {
+      //   bounds.extend(marker.getPosition());
+      // }
+
+      // map.fitBounds(bounds);
     });
   }
 }
@@ -108,13 +140,56 @@ async function getMeters(url) {
 }
 
 // Function to add info window to a marker
-function addInfoWindow(marker, data) {
+function addInfoWindow(marker, json) {
+  // Parse out relevant info from json data
+  // TODO:
+  // let weekdays = json.limit_and_rates.weekdays;
+  // let weekday_limits = [];
+  // let weekday_rates = [];
+  // let saturdays = json.limit_and_rates.saturday;
+  // let sat_limits = [];
+  // let sat_rates = [];
+  // let sundays = json.limit_and_rates.sunday;
+  // let sun_limits = [];
+  // let sun_rates = [];
+  // let meter_type = json.meter_type;
+  // let credit_card = json.credit_card;
+  // let pay_by_phone = json.pay_by_phone;
+  // let meter_id = json.meter_id;
+  // let in_effect = json.in_effect;
+
+  // for (let slot of weekdays) {
+  //   weekday_limits.push.apply(weekday_limits, [
+  //     slot.start,
+  //     slot.end,
+  //     slot.rate
+  //   ]);
+  // }
+  // console.log(weekday_limits);
+
+  let contentString = ReactDOMServer.renderToString(
+    <InfoWindow
+      key={json.meter_id}
+      weekdays={json.limits_and_rates.weekdays}
+      saturdays={json.limits_and_rates.saturday}
+      sundays={json.limits_and_rates.sunday}
+      meter_type={json.meter_type}
+      pay_by_phone={json.pay_by_phone}
+      credit_card={json.credit_card}
+    />
+  );
+
   // Attaching a click event to the current marker
+  var activeInfoWindow;
   var infowindow = new window.google.maps.InfoWindow({
-    content: data
+    content: contentString
   });
   marker.addListener("click", function() {
+    if (activeInfoWindow) {
+      activeInfoWindow.close();
+    }
     infowindow.open(map, marker);
+    activeInfoWindow = infowindow;
   });
 }
 
@@ -130,7 +205,7 @@ function addMarker(meter, map) {
     map: map
   });
 
-  addInfoWindow(marker, JSON.stringify(meter.properties));
+  addInfoWindow(marker, meter.properties);
   markers.push(marker);
 }
 
