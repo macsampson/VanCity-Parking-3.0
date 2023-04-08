4ef6916d5d0fbaa70e695042df01ba188bfa61ba
Mac <mac.sampson@pm.me>
2023-04-07 22:20:39 -0700

    add callback to sidebar for meter type

--- src/components/MeterTypeSelect.js
+++ src/components/MeterTypeSelect.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
 import InputLabel from '@mui/material/InputLabel'
@@ -6,28 +6,35 @@ import InputLabel from '@mui/material/InputLabel'
 import { FormControl } from '@mui/material'
 
 function MeterTypeSelect(props) {
-  return (
-    <FormControl style={{ marginTop: '16px' }}>
-      <InputLabel id="demo-simple-select-label">Meter Type</InputLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={props.meterType}
-        label="Meter Type"
-        onChange={props.onMeterChange}
-      >
-        <MenuItem value="Any">
-          <em>Any</em>
-        </MenuItem>
-        <MenuItem value={'Single'}>Single</MenuItem>
-        <MenuItem value={'Twin'}>Twin</MenuItem>
-        <MenuItem value={'Motorbike'}>Motorbike</MenuItem>
-        <MenuItem value={'Disability'}>Disability</MenuItem>
-        <MenuItem value={'Pay Station'}>Pay Station</MenuItem>
-        <MenuItem value={'Bay'}>Bay</MenuItem>
-      </Select>
-    </FormControl>
-  )
+	const [meterType, setMeterType] = useState('Any')
+
+	const handleChange = (event) => {
+		setMeterType(event.target.value)
+		props.onMeterChange(event.target.value)
+	}
+
+	return (
+		<FormControl style={{ marginTop: '16px' }}>
+			<InputLabel id="demo-simple-select-label">Meter Type</InputLabel>
+			<Select
+				labelId="demo-simple-select-label"
+				id="demo-simple-select"
+				value={meterType}
+				label="Meter Type"
+				onChange={handleChange}
+			>
+				<MenuItem value="Any">
+					<em>Any</em>
+				</MenuItem>
+				<MenuItem value={'Single'}>Single</MenuItem>
+				<MenuItem value={'Twin'}>Twin</MenuItem>
+				<MenuItem value={'Motorbike'}>Motorbike</MenuItem>
+				<MenuItem value={'Disability'}>Disability</MenuItem>
+				<MenuItem value={'Pay Station'}>Pay Station</MenuItem>
+				<MenuItem value={'Bay'}>Bay</MenuItem>
+			</Select>
+		</FormControl>
+	)
 }
 
 export default MeterTypeSelect
