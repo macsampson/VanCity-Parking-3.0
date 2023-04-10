1230f55fac94e239887e4b474d01f98791078d11
Mac <mac.sampson@pm.me>
2023-04-09 22:53:26 -0700

    add streetview, convert to flexbox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,49 @@
 import React, { useEffect, useState, useCallback, useMemo } from 'react'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
-
-const containerStyle = {
-	// width: '100%',
-	height: '100%',
-	position: 'absolute',
-	order: 2,
-	left: '405px',
-	right: '0px',
-}
+import {
+	GoogleMap,
+	useLoadScript,
+	Marker,
+	StreetViewPanorama,
+} from '@react-google-maps/api'
+import { height } from '@mui/system'
 
 const center = {
 	lat: 49.2827,
 	lng: -123.1207,
 }
+
+const mapStyle = {
+	width: '75%',
+	position: 'absolute',
+	maxHeight: '75%',
+	top: '0px',
+	left: '25%',
+	right: '0px',
+	bottom: '0px',
+}
+
+const dividerStyle = {
+	position: 'absolute',
+
+	width: '100%',
+	height: '2px',
+	top: 'calc(80%)',
+	left: '0px',
+	right: '0px',
+	backgroundColor: 'black',
+	cursor: 'ns-resize',
+}
+
+const streetViewStyle = {
+	position: 'absolute',
+	top: '75%',
+	left: '25%',
+	right: '0%',
+	bottom: '0%',
+}
+
+const clickedIcon = ''
+const parkingIcon = '/images/parking-meter.png'
 
 function Map(props) {
 	const { isLoaded, loadError } = useLoadScript({
@@ -26,6 +56,11 @@ function Map(props) {
 
 	// create state to hold markers
 	const [markers, setMarkers] = useState([])
+	// state to hold clicked marker
+	const [clickedMarker, setClickedMarker] = useState(null)
+
+	// state for location marker
+	const [locationMarker, setLocationMarker] = useState(null)
 
 	// update markers when props change
 	useEffect(() => {
@@ -34,6 +69,7 @@ function Map(props) {
 
 	// function to zoom map and pan smoothly to marker when clicked
 	function handleMarkerClick(marker) {
+		setClickedMarker(marker)
 		const newPosition = { lat: marker.lat, lng: marker.lng }
 		map.panTo(newPosition)
 
@@ -55,7 +91,7 @@ function Map(props) {
 				<Marker
 					key={marker.key}
 					position={position}
-					icon={'/images/parking-meter.png'}
+					icon={marker.clicked ? clickedIcon : parkingIcon}
 					onClick={() => handleMarkerClick(marker)}
 				/>
 			)
@@ -77,15 +113,57 @@ function Map(props) {
 	if (!isLoaded) return <p>Loading map</p>
 
 	return (
-		<GoogleMap
-			mapContainerStyle={containerStyle}
-			center={center}
-			zoom={15}
-			onLoad={onLoad}
-			// options={options}
-		>
-			{renderMarkers}
-		</GoogleMap>
+		<div style={{ flex: '0 0 75%' }}>
+			<div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+				<div style={{ flex: '1 1 75%' }}>
+					<GoogleMap
+						mapContainerStyle={mapStyle}
+						center={center}
+						zoom={15}
+						onLoad={onLoad}
+						options={{
+							streetViewControl: false,
+							mapTypeControl: false,
+							fullscreenControl: false,
+							styles: [
+								{
+									featureType: 'poi',
+									elementType: 'labels',
+									stylers: [{ visibility: 'off' }],
+								},
+							],
+						}}
+					>
+						{renderMarkers}
+					</GoogleMap>
+				</div>
+
+				<div className="streetview" style={{ flex: '1 1 25%' }}>
+					<GoogleMap
+						// add mapcontainerstyle to for 20% height starting from the bottom
+						mapContainerStyle={streetViewStyle}
+					>
+						<StreetViewPanorama
+							position={clickedMarker}
+							visible={true}
+							options={{
+								zoom: 1,
+								addressControl: false,
+								fullscreenControl: false,
+								motionTracking: false,
+								linksControl: false,
+								panControl: false,
+								enableCloseButton: false,
+								scrollwheel: false,
+								showRoadLabels: false,
+								bestGuess: true,
+								source: window.google.maps.StreetViewSource.OUTDOOR,
+							}}
+						/>
+					</GoogleMap>
+				</div>
+			</div>
+		</div>
 	)
 }
 
