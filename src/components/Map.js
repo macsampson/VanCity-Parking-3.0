446bb88735039c678191c1058e1591edde1f201c
Mac Sampson <mac.sampson@proton.me>
2023-05-06 14:24:53 -0700

    add sorting buttons and make meter state variable an array

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState, useCallback, useMemo } from 'react'
 import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  StreetViewPanorama,
-  //   DistanceMatrixService,
+	GoogleMap,
+	useLoadScript,
+	Marker,
+	StreetViewPanorama,
+	//   DistanceMatrixService,
 } from '@react-google-maps/api'
 // import { CloseFullscreen } from '@mui/icons-material'
 
 const center = {
-  lat: 49.2827,
-  lng: -123.1207,
+	lat: 49.2827,
+	lng: -123.1207,
 }
 
 // const dividerStyle = {
@@ -26,167 +26,167 @@ const center = {
 // }
 
 const streetViewStyle = {
-  position: 'absolute',
-  top: '75%',
-  left: '400px',
-  right: '0%',
-  bottom: '0%',
+	position: 'absolute',
+	top: '75%',
+	left: '400px',
+	right: '0%',
+	bottom: '0%',
 }
 
 const clickedIcon = '/images/clicked-meter.png'
 const parkingIcon = '/images/parking-meter.png'
 
 function Map(props) {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_MAPS_API,
-    libraries: props.libraries,
-  })
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: process.env.REACT_APP_MAPS_API,
+		libraries: props.libraries,
+	})
 
-  // create a state object to hold the map
-  const [map, setMap] = useState(null)
+	// create a state object to hold the map
+	const [map, setMap] = useState(null)
 
-  // create state to hold markers
-  const [markersData, setMarkersData] = useState([])
-  // state to hold clicked marker
-  const [clickedMarker, setClickedMarker] = useState(null)
-  // state to hold markers
-  const [markers, setMarkers] = useState([]) // probably dont need
+	// create state to hold markers
+	const [markersData, setMarkersData] = useState([])
+	// state to hold clicked marker
+	const [clickedMarker, setClickedMarker] = useState(null)
+	// state to hold markers
+	const [markers, setMarkers] = useState([]) // probably dont need
 
-  // state for location marker
-  //   const [locationMarker, setLocationMarker] = useState(null)
+	// state for location marker
+	//   const [locationMarker, setLocationMarker] = useState(null)
 
-  const mapStyle = {
-    width: 'auto',
-    position: 'absolute',
-    // maxHeight: '75%',
-    top: '0px',
-    left: '400px',
-    right: '0px',
-    bottom: clickedMarker ? '25%' : '0%',
-  }
+	const mapStyle = {
+		width: 'auto',
+		position: 'absolute',
+		// maxHeight: '75%',
+		top: '0px',
+		left: '400px',
+		right: '0px',
+		bottom: clickedMarker ? '25%' : '0%',
+	}
 
-  // use effect to console log when meter prop changes
-  useEffect(() => {
-    // console.log('meter changed', props.clickedMeter)
-    // console.log('markers', markers)
-    if (props.clickedMeter) {
-      handleMarkerClick(props.clickedMeter)
-    }
-  }, [props.clickedMeter])
+	// use effect to console log when meter prop changes
+	useEffect(() => {
+		// console.log('meter changed', props.clickedMeter)
+		// console.log('markers', markers)
+		if (props.clickedMeter) {
+			handleMarkerClick(props.clickedMeter)
+		}
+	}, [props.clickedMeter])
 
-  // update markers when props change
-  useEffect(() => {
-    // console.log('props changed', props.markers)
-    setMarkersData(props.markers)
-    // console.log('markers in map: ', markers)
-  }, [props.markers])
+	// update markers when props change
+	useEffect(() => {
+		// console.log('props changed', props.markers)
+		setMarkersData(props.markers)
+		// console.log('markers in map: ', markers)
+	}, [props.markers])
 
-  // function to zoom map and pan smoothly to marker when clicked
-  function handleMarkerClick(markerId) {
-    setClickedMarker(markerId)
-    const marker = markersData[markerId]
-    const newPosition = { lat: marker.lat, lng: marker.lng }
-    map.panTo(newPosition)
+	// function to zoom map and pan smoothly to marker when clicked
+	function handleMarkerClick(markerId) {
+		setClickedMarker(markerId)
+		const marker = markersData[markerId]
+		const newPosition = { lat: marker.lat, lng: marker.lng }
+		map.panTo(newPosition)
 
-    map.setZoom(20)
-    // props.onMarkerClicked(marker)
-  }
+		map.setZoom(20)
+		// props.onMarkerClicked(marker)
+	}
 
-  // function to get correct icon image depending if marker is clicked or not
-  //   const getIcon = (key) => {
-  //     console.log('clicked marker is ', clickedMarker)
-  //     console.log('key is ', key)
-  //     return key == clickedMarker ? clickedIcon : parkingIcon
-  //   }
+	// function to get correct icon image depending if marker is clicked or not
+	//   const getIcon = (key) => {
+	//     console.log('clicked marker is ', clickedMarker)
+	//     console.log('key is ', key)
+	//     return key == clickedMarker ? clickedIcon : parkingIcon
+	//   }
 
-  // Memoize the renderMarkers function to avoid unnecessary re-renders
-  const renderMarkers = useMemo(() => {
-    const bounds = new window.google.maps.LatLngBounds()
-    if (markersData.length === 0) {
-      console.log('no markers')
-      return null
-    }
-    // iterate through markers object and create a marker for each
+	// Memoize the renderMarkers function to avoid unnecessary re-renders
+	const renderMarkers = useMemo(() => {
+		const bounds = new window.google.maps.LatLngBounds()
+		if (markersData.length === 0) {
+			console.log('no markers')
+			return null
+		}
+		// iterate through markers object and create a marker for each
 
-    console.log('rendering markers')
-    const markersWithBounds = []
-    Object.keys(markersData).map((key) => {
-      const marker = markersData[key]
-      const position = { lat: marker.lat, lng: marker.lng }
-      //   console.log('position', position)
-      bounds.extend(position)
-      markersWithBounds.push({
-        key: (
-          <Marker
-            key={key}
-            position={position}
-            icon={key === props.clickedMeter ? clickedIcon : parkingIcon}
-            onClick={() => handleMarkerClick(key)}
-          />
-        ),
-      })
-    })
-    if (map) {
-      map.fitBounds(bounds)
-    }
-    setMarkers(markersWithBounds)
-  }, [markersData, map])
+		console.log('rendering markers')
+		const markersWithBounds = []
+		Object.keys(markersData).map((key) => {
+			const marker = markersData[key]
+			const position = { lat: marker.lat, lng: marker.lng }
+			//   console.log('position', position)
+			bounds.extend(position)
+			markersWithBounds.push({
+				key: (
+					<Marker
+						key={key}
+						position={position}
+						icon={key === props.clickedMeter ? clickedIcon : parkingIcon}
+						// onClick={() => handleMarkerClick(key)}
+					/>
+				),
+			})
+		})
+		if (map) {
+			map.fitBounds(bounds)
+		}
+		setMarkers(markersWithBounds)
+	}, [markersData, map])
 
-  // set the map state object to the map argument
-  const onLoad = useCallback(function callback(map) {
-    // const bounds = new window.google.maps.LatLngBounds()
-    // map.fitBounds(bounds)
-    setMap(map)
-  }, [])
+	// set the map state object to the map argument
+	const onLoad = useCallback(function callback(map) {
+		// const bounds = new window.google.maps.LatLngBounds()
+		// map.fitBounds(bounds)
+		setMap(map)
+	}, [])
 
-  if (loadError) return <p>Error loading map</p>
-  if (!isLoaded) return <p>Loading map</p>
+	if (loadError) return <p>Error loading map</p>
+	if (!isLoaded) return <p>Loading map</p>
 
-  return (
-    <div style={{ flex: '1' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-        <div style={{ flex: '1 1 auto' }}>
-          <GoogleMap
-            mapContainerStyle={mapStyle}
-            center={center}
-            zoom={15}
-            onLoad={onLoad}
-            options={{
-              streetViewControl: false,
-              mapTypeControl: false,
-              fullscreenControl: false,
-              styles: [
-                {
-                  featureType: 'poi',
-                  elementType: 'labels',
-                  stylers: [{ visibility: 'off' }],
-                },
-              ],
-            }}
-          >
-            {Object.keys(markersData).map((key) => (
-              <Marker
-                key={key}
-                position={{
-                  lat: markersData[key].lat,
-                  lng: markersData[key].lng,
-                }}
-                icon={key === props.clickedMeter ? clickedIcon : parkingIcon}
-                onClick={() => handleMarkerClick(key)}
-              />
-            ))}
-          </GoogleMap>
-        </div>
+	return (
+		<div style={{ flex: '1' }}>
+			<div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+				<div style={{ flex: '1 1 auto' }}>
+					<GoogleMap
+						mapContainerStyle={mapStyle}
+						center={center}
+						zoom={15}
+						onLoad={onLoad}
+						options={{
+							streetViewControl: false,
+							mapTypeControl: false,
+							fullscreenControl: false,
+							styles: [
+								{
+									featureType: 'poi',
+									elementType: 'labels',
+									stylers: [{ visibility: 'off' }],
+								},
+							],
+						}}
+					>
+						{Object.keys(markersData).map((key) => (
+							<Marker
+								key={key}
+								position={{
+									lat: markersData[key].lat,
+									lng: markersData[key].lng,
+								}}
+								icon={key === props.clickedMeter ? clickedIcon : parkingIcon}
+								onClick={() => handleMarkerClick(key)}
+							/>
+						))}
+					</GoogleMap>
+				</div>
 
-        {clickedMarker && (
-          <div className='streetview' style={{ flex: '1 1 25%' }}>
-            <GoogleMap
-              // add mapcontainerstyle to for 20% height starting from the bottom
-              mapContainerStyle={streetViewStyle}
-            >
-              {/* onyl show streetview if theres a clicked marker */}
+				{clickedMarker && (
+					<div className="streetview" style={{ flex: '1 1 25%' }}>
+						<GoogleMap
+							// add mapcontainerstyle to for 20% height starting from the bottom
+							mapContainerStyle={streetViewStyle}
+						>
+							{/* onyl show streetview if theres a clicked marker */}
 
-              {/* <StreetViewPanorama
+							{/* <StreetViewPanorama
                 position={markers[clickedMarker]}
                 visible={true}
                 options={{
@@ -203,12 +203,12 @@ function Map(props) {
                   source: window.google.maps.StreetViewSource.OUTDOOR,
                 }}
               /> */}
-            </GoogleMap>
-          </div>
-        )}
-      </div>
-    </div>
-  )
+						</GoogleMap>
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Map
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,173 +3,233 @@ import Box from '@mui/material/Box'
 import MeterInfo from './MeterInfo'
 import fetchParkingMeters from '../utils/FetchParkingMeters'
 import getDirections from '../utils/GetDirections'
+import { Button, ButtonGroup } from '@mui/material'
+import { render } from 'react-dom'
 
 export default function Sidebar(props) {
-  //   const [meterType, setMeterType] = useState('Any')
-  const [selectedPlace, setSelectedPlace] = useState(null)
-  const [markers, setMarkers] = useState({})
-  const [rawMeterInfo, setRawMeterInfo] = useState({})
-  const [currentMeterId, setCurrentMeterId] = useState(null)
-  const [currentMeters, setCurrentMeters] = useState(null)
-  // const [isLoading, setIsLoading] = useState(true)
-  // reference to meter info to scroll to
-  const meterInfoRef = useRef(null)
+	//   const [meterType, setMeterType] = useState('Any')
+	const [selectedPlace, setSelectedPlace] = useState(null)
+	const [markers, setMarkers] = useState({})
+	const [rawMeterInfo, setRawMeterInfo] = useState([])
+	const [currentMeterId, setCurrentMeterId] = useState(null)
+	const [currentMeters, setCurrentMeters] = useState([])
+	// const [isLoading, setIsLoading] = useState(true)
+	// reference to meter info to scroll to
+	const meterInfoRef = useRef(null)
 
-  // function to handle meter click
-  const handleMeterClick = (meter) => {
-    // console.log("meter clicked", meter);
-    props.clickedMeter(meter[0])
-    setCurrentMeterId(meter[0])
-  }
+	// function to handle meter click
+	const handleMeterClick = (meter) => {
+		// console.log("meter clicked", meter);
+		props.clickedMeter(meter[0])
+		setCurrentMeterId(meter[0])
+	}
 
-  useEffect(() => {
-    if (rawMeterInfo) {
-      // console.log(rawMeterInfo)
-      const newMarkers = {}
-      const newMeters = []
-      for (const key in rawMeterInfo) {
-        const meter = rawMeterInfo[key]
-        newMarkers[meter.meterid] = {
-          lng: meter.location.lng,
-          lat: meter.location.lat,
-        }
+	// function to handle sorting of meters
+	const handleSort = (sortType) => {
+		console.log(sortType)
+		const sortedCurrentMeters = [...currentMeters]
+		if (sortType === 'distance') {
+			sortedCurrentMeters.sort((a, b) => {
+				return a.duration - b.duration
+			})
+		}
+		setCurrentMeters(sortedCurrentMeters)
+	}
 
-        newMeters.push(
-          <MeterInfo
-            meter={meter}
-            expanded={
-              // check if meter.id exists in currentmeterid array
-              currentMeterId && currentMeterId === meter.meterid
-            }
-            key={meter.meterid}
-            meterClicked={handleMeterClick}
-          />
-        )
-      }
-      setMarkers(newMarkers)
-      setCurrentMeters(newMeters)
-    }
-  }, [rawMeterInfo])
+	// function to render meter info
+	const renderMeterInfo = () => {
+		if (currentMeters) {
+			return currentMeters.map((meter) => {
+				return (
+					<MeterInfo
+						key={meter.meterid}
+						meter={meter}
+						expanded={
+							// check if meter.id exists in currentmeterid array
+							currentMeterId && currentMeterId === meter.meterid
+						}
+						meterClicked={handleMeterClick}
+					/>
+				)
+			})
+		}
+	}
 
-  // use effect to set selected place from props
-  useEffect(() => {
-    setSelectedPlace(props.selectedPlace)
-  }, [props.selectedPlace])
+	useEffect(() => {
+		if (rawMeterInfo) {
+			// console.log(rawMeterInfo)
+			const newMeters = []
+			for (const key in rawMeterInfo) {
+				const meter = rawMeterInfo[key]
+				newMeters.push(meter)
+			}
+			setCurrentMeters(newMeters)
+		}
+	}, [rawMeterInfo])
 
-  useEffect(() => {
-    if (markers) {
-      //   console.log('markers leaving sidebar', markers)
-      props.onMarkersChange(markers)
-    }
-  }, [markers])
+	// use effect to set selected place from props
+	useEffect(() => {
+		setSelectedPlace(props.selectedPlace)
+	}, [props.selectedPlace])
 
-  // call the fetchParkingMeters function when place prop changes
-  useEffect(() => {
-    // console.log('selected place changed', selectedPlace)
-    if (selectedPlace) {
-      const fetchMeterInfo = async () => {
-        setRawMeterInfo(null)
-        try {
-          const meterData = await fetchParkingMeters(selectedPlace)
-          // console.log(data[0])
-          const durations = await getDirections(meterData, selectedPlace)
+	useEffect(() => {
+		if (markers) {
+			//   console.log('markers leaving sidebar', markers)
+			props.onMarkersChange(markers)
+		}
+	}, [markers])
 
-          // iterate through meter data and add duration to each meter
-          const dataWithDirections = {}
-          for (const key in meterData) {
-            const meter = meterData[key]
-            meter.duration =
-              durations[meter.location.lng + ',' + meter.location.lat].duration
-            dataWithDirections[meter.location.lng + ',' + meter.location.lat] =
-              meter
-          }
+	// call the fetchParkingMeters function when place prop changes
+	useEffect(() => {
+		// console.log('selected place changed', selectedPlace)
+		if (selectedPlace) {
+			const fetchMeterInfo = async () => {
+				setRawMeterInfo(null)
+				try {
+					const meterData = await fetchParkingMeters(selectedPlace)
+					// console.log(data[0])
+					const durations = await getDirections(meterData, selectedPlace)
 
-          setRawMeterInfo(dataWithDirections)
-          //   console.log(rawMeterInfo.data)
-        } catch (error) {
-          console.error(error)
-        } finally {
-          // setIsLoading(false)
-          console.log('finally returned')
-        }
-      }
-      fetchMeterInfo()
-    }
-  }, [selectedPlace])
+					// iterate through meter data and add duration to each meter
+					const dataWithDirections = {}
+					for (const key in meterData) {
+						const meter = meterData[key]
+						meter.duration =
+							durations[meter.location.lng + ',' + meter.location.lat].duration
+						dataWithDirections[
+							meter.location.lng + ',' + meter.location.lat
+						] = meter
+					}
 
-  // call findmeterinfo when marker is clicked
-  useEffect(() => {
-    // console.log('clicked')
-    if (props.clickedMarker) {
-      //   console.log('clicked', props.clickedMarker)
-      setCurrentMeterId(props.clickedMarker.key)
-      // expandMeterInfo(props.clickedMarker.key)
-    }
-  }, [props.clickedMarker])
+					//   console.log(rawMeterInfo.data)
+					const newMarkers = {}
+					for (const key in dataWithDirections) {
+						const meter = dataWithDirections[key]
+						newMarkers[meter.meterid] = {
+							lng: meter.location.lng,
+							lat: meter.location.lat,
+						}
+					}
+					setRawMeterInfo(dataWithDirections)
+					setMarkers(newMarkers)
+				} catch (error) {
+					console.error(error)
+				} finally {
+					// setIsLoading(false)
+					console.log('finally returned')
+				}
+			}
+			fetchMeterInfo()
+		}
+	}, [selectedPlace])
 
-  // useffect to update currentmeters when rawmeterinfo changes
-  useEffect(() => {
-    // console.log(rawMeterInfo);
+	// call findmeterinfo when marker is clicked
+	useEffect(() => {
+		// console.log('clicked')
+		if (props.clickedMarker) {
+			//   console.log('clicked', props.clickedMarker)
+			setCurrentMeterId(props.clickedMarker.key)
+			// expandMeterInfo(props.clickedMarker.key)
+		}
+	}, [props.clickedMarker])
 
-    if (currentMeterId) {
-      //   console.log('current meter id', currentMeterId)
-      meterInfoRef.current = document.getElementById(currentMeterId)
-      setTimeout(() => {
-        meterInfoRef.current.scrollIntoView({ behavior: 'smooth' })
-      }, 50)
-    }
-  }, [currentMeterId])
+	// useffect to update currentmeters when rawmeterinfo changes
+	useEffect(() => {
+		// console.log(rawMeterInfo);
 
-  // useffect to scroll to meter when meterinfo ref changes
-  // useEffect(() => {
-  // 	if (meterInfoRef.current) {
-  // 		setTimeout(() => {
-  // 			meterInfoRef.current.scrollIntoView({ behavior: 'smooth' })
-  // 		}, 50)
-  // 		// expand the meter info when it is scrolled to
-  // 		console.log(meterInfoRef.current)
-  // 	}
-  // }, [meterInfoRef.current])
+		if (currentMeterId) {
+			//   console.log('current meter id', currentMeterId)
+			meterInfoRef.current = document.getElementById(currentMeterId)
+			setTimeout(() => {
+				meterInfoRef.current.scrollIntoView({ behavior: 'smooth' })
+			}, 50)
+		}
+	}, [currentMeterId])
 
-  const styles = {
-    container: {
-      height: '100vh',
-      flex: '0 0 auto',
-      minWidth: '400px',
-      maxWidth: '400px',
-      background: '#e6e6e6',
-    },
-    search: {
-      // flex: '0 0 auto',
-      padding: '10px',
-      zIndex: 1,
-      position: 'absolute',
-      right: '0px',
-      width: '25%',
-      // width: '100%',
-    },
-  }
+	// useffect to scroll to meter when meterinfo ref changes
+	// useEffect(() => {
+	// 	if (meterInfoRef.current) {
+	// 		setTimeout(() => {
+	// 			meterInfoRef.current.scrollIntoView({ behavior: 'smooth' })
+	// 		}, 50)
+	// 		// expand the meter info when it is scrolled to
+	// 		console.log(meterInfoRef.current)
+	// 	}
+	// }, [meterInfoRef.current])
 
-  return (
-    <div className='sidebar-container' style={styles.container}>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          //   paddingBottom: "10px",
-        }}
-      >
-        <Box
-          style={{
-            flex: '0 0 100%',
-            overflow: 'scroll',
-            padding: '10px',
-          }}
-        >
-          {currentMeters}
-        </Box>
-      </div>
-    </div>
-  )
+	const styles = {
+		container: {
+			height: '92vh',
+			flex: '0 0 auto',
+			minWidth: '400px',
+			maxWidth: '400px',
+			background: '#e6e6e6',
+		},
+		search: {
+			// flex: '0 0 auto',
+			padding: '10px',
+			zIndex: 1,
+			position: 'absolute',
+			right: '0px',
+			width: '25%',
+			// width: '100%',
+		},
+		sorting: {
+			display: 'flex',
+			flexDirection: 'column',
+			// alignItems: 'center',
+			padding: '10px',
+			zIndex: 1,
+			// position: 'absolute',
+			left: '0px',
+			// width: '25%',
+			height: '8vh',
+			// width: '100%',
+		},
+		sortButtons: {
+			// flex: '1 1 auto',
+			justifyContent: 'center',
+			display: 'flex',
+		},
+		sortText: {
+			alignSelf: 'center',
+			marginRight: '10px',
+			flex: '1 1 auto',
+		},
+	}
+
+	return (
+		<div className="sidebar-container" style={styles.container}>
+			<div className="sorting-container" style={styles.sorting}>
+				<p style={styles.sortText}>Sort by:</p>
+				<div style={styles.sortButtons}>
+					<ButtonGroup
+						variant="contained"
+						aria-label="contained primary button group"
+					>
+						<Button onClick={() => handleSort('rate')}>Rate</Button>
+						<Button onClick={() => handleSort('distance')}>Distance</Button>
+					</ButtonGroup>
+				</div>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					height: '100%',
+					//   paddingBottom: "10px",
+				}}
+			>
+				<Box
+					style={{
+						flex: '0 0 100%',
+						overflow: 'scroll',
+						padding: '10px',
+					}}
+				>
+					{renderMeterInfo()}
+				</Box>
+			</div>
+		</div>
+	)
 }
