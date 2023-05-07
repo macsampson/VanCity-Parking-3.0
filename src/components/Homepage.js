f24cd5cd302ee918f50060891233057f5be29dbb
Mac Sampson <mac.sampson@proton.me>
2023-05-07 10:01:22 -0700

    add top bar and change app name

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -14,58 +14,57 @@ const key = process.env.REACT_APP_MAPS_API
 const libraries = ['places']
 
 function Homepage() {
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 
-  const handleSelectedPlace = (place) => {
-    // push the lat and lng from place to the navigation state
-    navigate('/parking', {
-      state: {
-        lat: place.geometry.location.lat(),
-        lng: place.geometry.location.lng(),
-      },
-    })
-  }
+	const handleSelectedPlace = (place) => {
+		// push the lat and lng from place to the navigation state
+		navigate('/parking', {
+			state: {
+				lat: place.geometry.location.lat(),
+				lng: place.geometry.location.lng(),
+			},
+		})
+	}
 
-  const styles = {
-    title: {
-      color: 'white',
-    },
-    subtitle: {
-      color: 'white',
-    },
-  }
+	const styles = {
+		title: {
+			color: 'white',
+			fontSize: '5rem',
+			fontFamily: 'Roboto',
+		},
+		subtitle: {
+			color: 'white',
+			fontSize: '1.5rem',
+			fontFamily: 'Roboto',
+		},
+	}
 
-  return (
-    <LoadScript googleMapsApiKey={key} libraries={libraries}>
-      <div
-        className='homepage'
-        style={{
-          // use bg.jpg image from images folder as background
-          backgroundImage: `url(${process.env.PUBLIC_URL}/images/bg.jpg)`,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
-        <h1 style={styles.title}>
-          Vancouver Parking{' '}
-          <span role={'img'} aria-label={'parking icon'}>
-            🅿️
-          </span>
-        </h1>
-        <p style={styles.subtitle}>Find parking near you</p>
-        <div
-          style={{
-            width: '50vw',
-          }}
-        >
-          <SearchBar onSelectPlace={handleSelectedPlace} />
-        </div>
-      </div>
-    </LoadScript>
-  )
+	return (
+		<LoadScript googleMapsApiKey={key} libraries={libraries}>
+			<div
+				className="homepage"
+				style={{
+					// use bg.jpg image from images folder as background
+					backgroundImage: `url(${process.env.PUBLIC_URL}/images/bg.jpg)`,
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}
+			>
+				<h1 style={styles.title}>ParkSmart </h1>
+				<p style={styles.subtitle}>Find parking in Vancouver</p>
+				<div
+					style={{
+						width: '50vw',
+					}}
+				>
+					<SearchBar onSelectPlace={handleSelectedPlace} />
+				</div>
+			</div>
+		</LoadScript>
+	)
 }
 
 export default Homepage
--- src/components/ParkingPage.js
+++ src/components/ParkingPage.js
@@ -6,6 +6,9 @@ import { LoadScript } from '@react-google-maps/api'
 import { register as registerServiceWorker } from '../serviceWorker'
 import { useLocation } from 'react-router-dom'
 import '../styles/ParkingPage.css'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import { useNavigate } from 'react-router-dom'
 
 const dotenv = require('dotenv')
 dotenv.config()
@@ -15,79 +18,95 @@ const key = process.env.REACT_APP_MAPS_API
 const libraries = ['places']
 
 export default function ParkingPage() {
-  const [markers, setMarkers] = useState({})
-  const [clickedMarker, setClickedMarker] = useState(null)
-  const [selectedPlace, setSelectedPlace] = useState(null)
-  const [meter, setMeter] = useState(null)
+	const [markers, setMarkers] = useState({})
+	const [clickedMarker, setClickedMarker] = useState(null)
+	const [selectedPlace, setSelectedPlace] = useState(null)
+	const [meter, setMeter] = useState(null)
 
-  const location = useLocation()
-  const { state } = location
+	const location = useLocation()
+	const { state } = location
 
-  useEffect(() => {
-    registerServiceWorker()
-  }, [])
+	const navigate = useNavigate()
 
-  // handle when a marker is clicked on the map
-  const handleMarkerClick = (marker) => {
-    // console.log('marker clicked', marker.key)
-    setClickedMarker(marker)
-  }
+	useEffect(() => {
+		registerServiceWorker()
+	}, [])
 
-  const handleMeterClick = (meter) => {
-    // console.log("meter clicked", meter);
-    setMeter(meter)
-  }
+	// handle when a marker is clicked on the map
+	const handleMarkerClick = (marker) => {
+		// console.log('marker clicked', marker.key)
+		setClickedMarker(marker)
+	}
 
-  // handle selected place from searchbar
-  const handleSelectedPlace = (place) => {
-    // console.log('place selected', place)
-    // get lat and lng from place
-    if (place.geometry) {
-      const lat = place.geometry.location.lat()
-      const lng = place.geometry.location.lng()
-      // set selected place
-      setSelectedPlace({ lat, lng })
-    }
-  }
+	const handleMeterClick = (meter) => {
+		// console.log("meter clicked", meter);
+		setMeter(meter)
+	}
 
-  // use effect to set selected place when it changes
-  useEffect(() => {
-    if (state) {
-      setSelectedPlace(state)
-    }
-  }, [state])
+	// handle selected place from searchbar
+	const handleSelectedPlace = (place) => {
+		// console.log('place selected', place)
+		// get lat and lng from place
+		if (place.geometry) {
+			const lat = place.geometry.location.lat()
+			const lng = place.geometry.location.lng()
+			// set selected place
+			setSelectedPlace({ lat, lng })
+		}
+	}
 
-  const styles = {
-    search: {
-      // flex: '0 0 auto',
-      padding: '10px',
-      zIndex: 1,
-      position: 'absolute',
-      right: '0px',
-      width: '50vw',
-    },
-  }
+	const goHome = () => {
+		navigate('/')
+	}
 
-  return (
-    <LoadScript googleMapsApiKey={key} libraries={libraries}>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        {/* <SearchBar onSelectedPlace={setSelectedPlace} /> */}{' '}
-        <div className='searchbar-container' style={styles.search}>
-          <Searchbar onSelectPlace={handleSelectedPlace} />
-        </div>
-        <Sidebar
-          onMarkersChange={setMarkers}
-          clickedMarker={clickedMarker}
-          clickedMeter={handleMeterClick}
-          selectedPlace={selectedPlace}
-        ></Sidebar>
-        <Map
-          markers={markers}
-          onMarkerClicked={handleMarkerClick}
-          clickedMeter={meter}
-          selectedPlace={selectedPlace}
-        />
-      </div>
-    </LoadScript>
-  )
+	// use effect to set selected place when it changes
+	useEffect(() => {
+		if (state) {
+			setSelectedPlace(state)
+		}
+	}, [state])
+
+	const styles = {
+		search: {
+			// flex: '0 0 auto',
+			padding: '10px',
+			zIndex: 1,
+			position: 'absolute',
+			right: '0px',
+			width: '50vw',
+		},
+	}
+
+	return (
+		<LoadScript googleMapsApiKey={key} libraries={libraries}>
+			<AppBar
+				position="static"
+				style={{ backgroundColor: 'rgb(81, 209, 251)', zIndex: '1' }}
+			>
+				<Toolbar style={{ display: 'flex', justifyContent: 'left' }}>
+					<h1 style={{ color: 'white', cursor: 'pointer' }} onClick={goHome}>
+						ParkSmart
+					</h1>
+				</Toolbar>
+			</AppBar>
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				{/* <SearchBar onSelectedPlace={setSelectedPlace} /> */}{' '}
+				<div className="searchbar-container" style={styles.search}>
+					<Searchbar onSelectPlace={handleSelectedPlace} />
+				</div>
+				<Sidebar
+					onMarkersChange={setMarkers}
+					clickedMarker={clickedMarker}
+					clickedMeter={handleMeterClick}
+					selectedPlace={selectedPlace}
+				></Sidebar>
+				<Map
+					markers={markers}
+					onMarkerClicked={handleMarkerClick}
+					clickedMeter={meter}
+					selectedPlace={selectedPlace}
+				/>
+			</div>
+		</LoadScript>
+	)
 }
