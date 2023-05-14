39e3f2967fb0aa251af13e60e181efa81dd83886
Mac Sampson <mac.sampson@proton.me>
2023-05-14 14:30:07 -0700

    fix map over rendering, searchbar placement, and panning

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState, useCallback, useMemo } from 'react'
 import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  StreetViewPanorama,
-  //   DistanceMatrixService,
+	GoogleMap,
+	useLoadScript,
+	Marker,
+	StreetViewPanorama,
+	//   DistanceMatrixService,
 } from '@react-google-maps/api'
 
 import '../styles/Map.css'
@@ -13,8 +13,8 @@ const key = process.env.REACT_APP_MAPS_API
 const libraries = ['places']
 
 const center = {
-  lat: 49.2827,
-  lng: -123.1207,
+	lat: 49.2827,
+	lng: -123.1207,
 }
 
 // const clickedIcon = '/images/clicked-meter.png'
@@ -24,141 +24,155 @@ const destinationIcon = '/images/destination.png'
 // const flagIcon = '/images/flag.png'
 
 function Map(props) {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: key,
-    libraries: libraries,
-  })
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: key,
+		libraries: libraries,
+	})
 
-  const [map, setMap] = useState(null)
-  const [markersData, setMarkersData] = useState([])
-  const [clickedMarker, setClickedMarker] = useState(null)
-  // const [markers, setMarkers] = useState([]) // probably dont need
-  const [selectedPlace, setSelectedPlace] = useState(null)
-  const [locationMarker, setLocationMarker] = useState(null)
+	const [map, setMap] = useState(null)
+	const [markersData, setMarkersData] = useState([])
+	const [clickedMarker, setClickedMarker] = useState(null)
+	// const [markers, setMarkers] = useState([]) // probably dont need
+	const [selectedPlace, setSelectedPlace] = useState(null)
+	const [locationMarker, setLocationMarker] = useState(null)
 
-  // useffect to update selected place when it changes
+	// useffect to update selected place when it changes
 
-  useEffect(() => {
-    if (props.selectedPlace) {
-      setLocationMarker(
-        <Marker position={props.selectedPlace} icon={destinationIcon} />
-      )
-      setSelectedPlace(props.selectedPlace)
-      setClickedMarker(null)
-    }
-  }, [props.selectedPlace])
+	useEffect(() => {
+		if (props.selectedPlace) {
+			// setLocationMarker(
+			// 	<Marker position={props.selectedPlace} icon={destinationIcon} />
+			// )
+			setSelectedPlace(props.selectedPlace)
+			setClickedMarker(null)
+		}
+	}, [props.selectedPlace])
 
-  // use effect to console log when meter prop changes
-  useEffect(() => {
-    // console.log('meter changed', props.clickedMeter)
-    // console.log('markers', markers)
-    if (props.clickedMeter) {
-      handleMarkerClick(props.clickedMeter)
-    }
-  }, [props.clickedMeter])
+	// use effect to console log when meter prop changes
+	useEffect(() => {
+		// console.log('meter changed', props.clickedMeter)
+		// console.log('markers', markers)
+		if (props.clickedMeter) {
+			handleMarkerClick(props.clickedMeter)
+		}
+	}, [props.clickedMeter])
 
-  // update markers when props change
-  useEffect(() => {
-    // console.log('props changed', props.markers)
-    setMarkersData(props.markers)
-    // console.log('markers in map: ', markers)
-  }, [props.markers])
+	// update markers when props change
+	useEffect(() => {
+		// console.log('props changed', props.markers)
+		setMarkersData(props.markers)
+		// console.log('markers in map: ', markers)
+	}, [props.markers])
 
-  // function to zoom map and pan smoothly to marker when clicked
-  function handleMarkerClick(markerId) {
-    setClickedMarker(markerId)
-    const marker = markersData[markerId]
-    const newPosition = { lat: marker.lat, lng: marker.lng }
-    map.panTo(newPosition)
-    // map.setZoom(19)
-    props.onMarkerClicked(markerId)
-  }
+	// useffect to pan to clicked marker when clicked
+	useEffect(() => {
+		if (clickedMarker) {
+			const marker = markersData[clickedMarker]
+			const newPosition = { lat: marker.lat, lng: marker.lng }
+			map.panTo(newPosition)
+			// console.log('panning to:', newPosition)
+			map.setZoom(19)
+		}
+	}, [clickedMarker])
 
-  const renderMarkers =
-    (() => {
-      if (map) {
-        const bounds = new window.google.maps.LatLngBounds()
-        let markers = []
-        console.log('rendering markers')
-        // iteraate through markersData object and create markers
-        Object.entries(markersData).forEach(([key, marker]) => {
-          const position = { lat: marker.lat, lng: marker.lng }
-          bounds.extend(position)
-          markers.push(
-            <Marker
-              key={key}
-              position={position}
-              icon={key === clickedMarker ? clickedIcon2 : parkingIcon}
-              onClick={() => handleMarkerClick(key)}
-            />
-          )
-        })
-        bounds.extend(selectedPlace)
-        markers.push(locationMarker)
-        map.fitBounds(bounds)
-        return markers
-      }
-    })() || []
+	// function to zoom map and pan smoothly to marker when clicked
+	function handleMarkerClick(markerId) {
+		setClickedMarker(markerId)
+		props.onMarkerClicked(markerId)
+	}
 
-  // set the map state object to the map argument
-  const onLoad = useCallback((map) => {
-    setMap(map)
-  }, [])
+	function getMarkers() {
+		if (map) {
+			const bounds = new window.google.maps.LatLngBounds()
+			let markers = []
+			// console.log('rendering markers')
+			// iterate through markersData object and create markers
+			Object.entries(markersData).forEach(([key, marker]) => {
+				const position = { lat: marker.lat, lng: marker.lng }
+				bounds.extend(position)
+				markers.push(
+					<Marker
+						key={key}
+						position={position}
+						icon={key === clickedMarker ? clickedIcon2 : parkingIcon}
+						onClick={() => handleMarkerClick(key)}
+					/>
+				)
+			})
+			if (selectedPlace) {
+				markers.push(<Marker position={selectedPlace} icon={destinationIcon} />)
+				bounds.extend(selectedPlace)
+			}
+			map.fitBounds(bounds)
+			return markers
+		}
+	}
 
-  if (loadError) return <p>Error loading map</p>
-  if (!isLoaded) return <p>Loading map</p>
+	const renderMarkers = useMemo(() => getMarkers(), [
+		markersData,
+		clickedMarker,
+		selectedPlace,
+	])
 
-  return (
-    isLoaded && (
-      <div className={props.className}>
-        <GoogleMap
-          mapContainerClassName='map'
-          // mapContainerStyle={mapStyle}
-          center={center}
-          // zoom={15}
-          onLoad={onLoad}
-          options={{
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-            zoomControl: false,
-            styles: [
-              {
-                featureType: 'poi',
-                elementType: 'labels',
-                stylers: [{ visibility: 'off' }],
-              },
-            ],
-          }}
-        >
-          {renderMarkers}
-        </GoogleMap>
+	// set the map state object to the map argument
+	const onLoad = useCallback((map) => {
+		setMap(map)
+	}, [])
 
-        {clickedMarker && (
-          <GoogleMap mapContainerClassName='street-view'>
-            <StreetViewPanorama
-              position={markersData[clickedMarker]}
-              visible={true}
-              options={{
-                zoom: 0,
-                addressControl: false,
-                fullscreenControl: false,
-                motionTracking: false,
-                zoomControl: false,
-                linksControl: false,
-                panControl: false,
-                enableCloseButton: false,
-                scrollwheel: true,
-                showRoadLabels: false,
-                bestGuess: true,
-                source: window.google.maps.StreetViewSource.OUTDOOR,
-              }}
-            />
-          </GoogleMap>
-        )}
-      </div>
-    )
-  )
+	if (loadError) return <p>Error loading map</p>
+	if (!isLoaded) return <p>Loading map</p>
+
+	return (
+		isLoaded && (
+			<div className={props.className}>
+				<GoogleMap
+					mapContainerClassName="map"
+					// mapContainerStyle={mapStyle}
+					center={center}
+					// zoom={15}
+					onLoad={onLoad}
+					options={{
+						streetViewControl: false,
+						mapTypeControl: false,
+						fullscreenControl: false,
+						zoomControl: false,
+						styles: [
+							{
+								featureType: 'poi',
+								elementType: 'labels',
+								stylers: [{ visibility: 'off' }],
+							},
+						],
+					}}
+				>
+					{renderMarkers}
+				</GoogleMap>
+
+				{clickedMarker && (
+					<GoogleMap mapContainerClassName="street-view">
+						<StreetViewPanorama
+							position={markersData[clickedMarker]}
+							visible={true}
+							options={{
+								zoom: 0,
+								addressControl: false,
+								fullscreenControl: false,
+								motionTracking: false,
+								zoomControl: false,
+								linksControl: false,
+								panControl: false,
+								enableCloseButton: false,
+								scrollwheel: true,
+								showRoadLabels: false,
+								bestGuess: true,
+								source: window.google.maps.StreetViewSource.OUTDOOR,
+							}}
+						/>
+					</GoogleMap>
+				)}
+			</div>
+		)
+	)
 }
 
 export default Map
--- src/components/ParkingPage.js
+++ src/components/ParkingPage.js
@@ -14,97 +14,101 @@ const key = process.env.REACT_APP_MAPS_API
 const libraries = ['places']
 
 export default function ParkingPage() {
-  const [markers, setMarkers] = useState({})
-  const [clickedMarker, setClickedMarker] = useState(null)
-  const [selectedPlace, setSelectedPlace] = useState(null)
-  const [meter, setMeter] = useState(null)
-  const [listOrMap, setListOrMap] = useState('map-views')
+	const [markers, setMarkers] = useState({})
+	const [clickedMarker, setClickedMarker] = useState(null)
+	const [selectedPlace, setSelectedPlace] = useState(null)
+	const [meter, setMeter] = useState(null)
+	const [listOrMap, setListOrMap] = useState('map')
 
-  const location = useLocation()
-  const { state } = location
+	const location = useLocation()
+	const { state } = location
 
-  const navigate = useNavigate()
-  const goHome = useCallback(() => navigate('/'), [navigate])
+	const navigate = useNavigate()
+	const goHome = useCallback(() => navigate('/'), [navigate])
 
-  useEffect(() => {
-    registerServiceWorker()
-  }, [])
+	useEffect(() => {
+		registerServiceWorker()
+	}, [])
 
-  useEffect(() => {
-    if (state) {
-      setSelectedPlace(state)
-    }
-  }, [state])
+	useEffect(() => {
+		if (state) {
+			setSelectedPlace(state)
+		}
+	}, [state])
 
-  const handleMarkerClick = useCallback((marker) => {
-    setClickedMarker(marker)
-  }, [])
+	const handleMarkerClick = useCallback((marker) => {
+		setClickedMarker(marker)
+	}, [])
 
-  const handleMeterClick = useCallback((meter) => {
-    setMeter(meter)
-  }, [])
+	const handleMeterClick = useCallback((meter) => {
+		setMeter(meter)
+	}, [])
 
-  const handleSelectedPlace = useCallback((place) => {
-    if (place.geometry) {
-      const lat = place.geometry.location.lat()
-      const lng = place.geometry.location.lng()
-      setSelectedPlace({ lat, lng })
-    }
-  }, [])
+	const handleSelectedPlace = useCallback((place) => {
+		if (place.geometry) {
+			const lat = place.geometry.location.lat()
+			const lng = place.geometry.location.lng()
+			setSelectedPlace({ lat, lng })
+		}
+	}, [])
 
-  const handleListButtonClick = useCallback(() => {
-    setListOrMap('list')
-  }, [])
+	const handleListButtonClick = useCallback(() => {
+		setListOrMap('list')
+	}, [])
 
-  const handleMapButtonClick = useCallback(() => {
-    setListOrMap('map-views')
-  }, [])
+	const handleMapButtonClick = useCallback(() => {
+		setListOrMap('map')
+	}, [])
 
-  return (
-    <LoadScript googleMapsApiKey={key} libraries={libraries}>
-      <div className='parking-page'>
-        <div className='title'>
-          <span onClick={goHome}>
-            <h1>ParkSmart</h1>
-          </span>
-        </div>
+	return (
+		<LoadScript googleMapsApiKey={key} libraries={libraries}>
+			<div className="parking-page">
+				<div className="title">
+					<span onClick={goHome}>
+						<h1>ParkSmart</h1>
+					</span>
+				</div>
 
-        <div className='body-container'>
-          <Searchbar
-            className={
-              listOrMap === 'map-views'
-                ? 'searchbar-container'
-                : 'searchbar-container-hidden'
-            }
-            onSelectPlace={handleSelectedPlace}
-          />
-          <Sidebar
-            className={listOrMap === 'list' ? 'sidebar' : 'sidebar-hidden'}
-            onMarkersChange={setMarkers}
-            clickedMarker={clickedMarker}
-            clickedMeter={handleMeterClick}
-            selectedPlace={selectedPlace}
-          ></Sidebar>
-          <Map
-            className={
-              listOrMap === 'map-views' ? 'map-views' : 'map-views-hidden'
-            }
-            markers={markers}
-            onMarkerClicked={handleMarkerClick}
-            clickedMeter={meter}
-            selectedPlace={selectedPlace}
-          />
+				<div className="body-container">
+					<Sidebar
+						className={listOrMap === 'list' ? 'sidebar' : 'sidebar-hidden'}
+						onMarkersChange={setMarkers}
+						clickedMarker={clickedMarker}
+						clickedMeter={handleMeterClick}
+						selectedPlace={selectedPlace}
+					></Sidebar>
+					<div
+						className={
+							listOrMap === 'map' ? 'map-container' : 'map-container-hidden'
+						}
+					>
+						<Searchbar
+							className={
+								listOrMap === 'map'
+									? 'searchbar-container'
+									: 'searchbar-container-hidden'
+							}
+							onSelectPlace={handleSelectedPlace}
+						/>
+						<Map
+							className={'map-views'}
+							markers={markers}
+							onMarkerClicked={handleMarkerClick}
+							clickedMeter={meter}
+							selectedPlace={selectedPlace}
+						/>
+					</div>
 
-          <ButtonGroup
-            className='mobile-views'
-            variant='contained'
-            aria-label='outlined button group'
-          >
-            <Button onClick={handleListButtonClick}>List</Button>
-            <Button onClick={handleMapButtonClick}>Map</Button>
-          </ButtonGroup>
-        </div>
-      </div>
-    </LoadScript>
-  )
+					<ButtonGroup
+						className="mobile-views"
+						variant="contained"
+						aria-label="outlined button group"
+					>
+						<Button onClick={handleListButtonClick}>List</Button>
+						<Button onClick={handleMapButtonClick}>Map</Button>
+					</ButtonGroup>
+				</div>
+			</div>
+		</LoadScript>
+	)
 }
